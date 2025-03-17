423ffefbe87cbd35189b8ff8444249c2f7d8d317
gaojunkang <gaojunkang@xiaohongshu.com>
2025-03-17 16:57:57 +0800

    ci: build script

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-podman buildx build --platform linux/amd64 -t ccr.ccs.tencentyun.com/ling-default-1/ling-sysint:alpha-1-x86 .
+
+GIT_HASH=$(git rev-parse --short HEAD)
+
+echo "Building ling-sysint:$GIT_HASH"
+
+# ccr.ccs.tencentyun.com/ling-default-1/ling-sysint
+podman buildx build --platform linux/amd64 -t ccr.ccs.tencentyun.com/ling-default-1/ling-sysint:$GIT_HASH -f Dockerfile .
+podman push ccr.ccs.tencentyun.com/ling-default-1/ling-sysint:$GIT_HASH
+
